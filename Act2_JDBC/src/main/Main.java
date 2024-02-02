795ebe634759ba5c5b8eb3d4cd113e90e2cc145e
sergiopalmero <sergiopalmero87@gmail.com>
2024-02-02 13:54:32 +0100

    Prueba exitosa conectar BBDD

--- Act2_JDBC/src/main/Main.java
+++ Act2_JDBC/src/main/Main.java
@@ -1,8 +1,27 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		// Paso 1: Establecer conexi�n con la base de datos
+				String cadenaConexion = "jdbc:mysql://localhost:3306/Act2_Conectores";
+				String user = "root";
+				String pass = "";
+				Connection con;
+				try {
+					con = DriverManager.getConnection(cadenaConexion, user, pass);
+				} catch (SQLException e) {
+					System.out.println("No se ha podido establecer la conexion con la BD");
+					System.out.println(e.getMessage());
+					return;
+				}
+				System.out.println("Se ha establecido la conexion con la Base de datos");
+				
 		
 	}
 
